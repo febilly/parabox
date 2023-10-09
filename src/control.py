cc9a835a2b1cc45b1470723e958a0bc3a00ee4d4
febilly <fake@fake.com>
2023-10-09 15:59:26 +0800

    no, this one

--- src/control.py
+++ src/control.py
@@ -3,6 +3,7 @@ import hpprime
 
 KEY_MAPPING = {2: actions.UP, 7: actions.LEFT, 12: actions.DOWN, 8: actions.RIGHT,
                18: actions.UP, 23: actions.LEFT, 24: actions.DOWN, 25: actions.RIGHT,
+               39: actions.UP, 14: actions.LEFT, 34: actions.DOWN, 17: actions.RIGHT,
                19: actions.UNDO, 49: actions.UNDO, 4: actions.RESTART, 30: actions.ENTER}
 
 def get_input(wait: bool = True) -> int:
